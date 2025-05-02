db277ab21b3d109cd2f63497c134ce0571d2960e
Matthew Chapman <chappersthepenguin@gmail.com>
2025-05-02 19:51:20 +0100

    Update config.py

--- analysis/config.py
+++ analysis/config.py
@@ -1,30 +1,30 @@
 from pathlib import Path
 
 # IO PATHS
-ZIP_INPUT_DIR = Path(r"Zip")
-EXTRACTED_OUTPUT_DIR = Path(r"Extracted")
-RESULTS_CSV = Path(r"results.csv")
+ZIP_INPUT_DIR = Path(r"Zip")  # Path containing zipped data
+EXTRACTED_OUTPUT_DIR = Path(r"Extracted")  # Path to extract zipped data to
+# RESULTS_CSV = Path(r"results.csv") 
 
 # RUNTIME OPTIONS
-POSE_BACKENDS_TO_RUN = ["blazepose", "openpose", "hrnet"]
-POSE_BACKENDS = {
+POSE_BACKENDS_TO_RUN = ["blazepose", "openpose", "hrnet"]  # Pose Backends to Run
+POSE_BACKENDS = {  # Dictionary containing each BASMI metric and the models that can calculate it
     "cr": ["blazepose"],
     "lsf": ["blazepose", "hrnet", "openpose"],
     "imd": ["blazepose", "hrnet", "openpose"],
     "ttw": ["blazepose"],
 }
-OUTPUT_CSV = Path("output.csv")
-SMOOTHING = True
+OUTPUT_CSV = Path("output.csv")  # The output CSV path
+SMOOTHING = True  # Whether to apply a Savitzky–Golay filter to the data
 
-SG_WINDOW_LENGTH = 9  # must be odd
-SG_POLY_ORDER = 2
-OUTLIER_ZSCORE = 3.0
+SG_WINDOW_LENGTH = 9  # must be odd, used for smoothing
+SG_POLY_ORDER = 2  # used for smoothing
+OUTLIER_ZSCORE = 3.0  # zscore for outlier removal
 
 assert SG_WINDOW_LENGTH % 2 == 1, "SG_WINDOW_LENGTH must be odd."
 
 # HRNET
-HRNET_ROOT = Path(r"hrnet")
+HRNET_ROOT = Path(r"hrnet")  # Root HRNET directory
 
 # OPENPOSE
-OPENPOSE_ROOT = Path("openpose")
-OPENPOSE_BIN = Path(OPENPOSE_ROOT / "bin")
+OPENPOSE_ROOT = Path("openpose")  # Root OpenPose Directory
+OPENPOSE_BIN = Path(OPENPOSE_ROOT / "bin")  # Path to OpenPose binaries folder (containing OpenPoseDemo.exe)
