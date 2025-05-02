70ce2dcc9834e83a2c81126ae87d7fc375b2caf2
Matthew Chapman <chappersthepenguin@gmail.com>
2025-05-02 19:01:12 +0100

    Update config.py

--- analysis/config.py
+++ analysis/config.py
@@ -6,12 +6,14 @@ EXTRACTED_OUTPUT_DIR = Path(r"Extracted")
 RESULTS_CSV = Path(r"results.csv")
 
 # RUNTIME OPTIONS
+POSE_BACKENDS_TO_RUN = ["blazepose", "openpose", "hrnet"]
 POSE_BACKENDS = {
     "cr": ["blazepose"],
     "lsf": ["blazepose", "hrnet", "openpose"],
     "imd": ["blazepose", "hrnet", "openpose"],
     "ttw": ["blazepose"],
 }
+OUTPUT_CSV = Path("output.csv")
 SMOOTHING = True
 
 SG_WINDOW_LENGTH = 9  # must be odd
@@ -20,5 +22,9 @@ OUTLIER_ZSCORE = 3.0
 
 assert SG_WINDOW_LENGTH % 2 == 1, "SG_WINDOW_LENGTH must be odd."
 
-# WEIGHTS
-WEIGHTS_DIR = Path(r"weights")
+# HRNET
+HRNET_ROOT = Path(r"hrnet")
+
+# OPENPOSE
+OPENPOSE_ROOT = Path("openpose")
+OPENPOSE_BIN = Path(OPENPOSE_ROOT / "bin")
